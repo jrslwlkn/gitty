e53f9fa3ff6ec634f2c18bdf9c40d70eb9914cc6
Yareeque <jrslwlkn@gmail.com>
2022-05-23 19:20:07 -0500

    Add some helper functions

--- extension.js
+++ extension.js
@@ -8,6 +8,44 @@ function exec(cmd, opts = {}) {
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require("vscode");
 
+const _cwd = () => vscode.workspace.workspaceFolders[0].uri.fsPath;
+
+const _str2arr = (str) => {
+	return str
+		.split("\n")
+		.filter((x) => x.trim())
+		.map((x) => x.trim().split(" ")[0]);
+};
+
+const _exec = async (cmd, opts = {}) => {
+	const s = await exec(cmd, { ...opts, cwd: opts.cwd || _cwd() });
+	return _str2arr(s);
+};
+
+const _wtrees = async () => await _exec("git worktree list");
+
+const _branches = async () => await _exec("git branch -a");
+
+const _wt_cur_dir = async () => {
+	const res = await _exec("git rev-parse --show-toplevel");
+	return res[0];
+};
+
+const _wt_base_dir = async () => {
+	const s = await _wt_cur_dir();
+	return s.replace(/\/[^\/]+$/g, "").trim();
+};
+
+const _ui_alert = (s) => vscode.window.showInformationMessage(s);
+
+const _ui_input = async (placeHolder) => {
+	return vscode.window.showInputBox({ placeHolder });
+};
+
+const _ui_select = async (placeHolder, options) => {
+	return vscode.window.showQuickPick(options, { placeHolder });
+};
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 
@@ -29,9 +67,12 @@ function activate(context) {
 		vscode.window.showInformationMessage("Hello World from gitty!");
 	});
 
-	const addWT = vscode.commands.registerCommand("G");
+	const addWT = vscode.commands.registerCommand("gitty.add_worktree", async () => {
+		const branches = _branches();
 
-	context.subscriptions.push(disposable);
+	});
+
+	context.subscriptions.push(disposable, addWT);
 }
 
 // this method is called when your extension is deactivated
